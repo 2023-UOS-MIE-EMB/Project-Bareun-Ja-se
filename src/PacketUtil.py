4f2b55820204efbc982ff7af83d2d895603678d5
OliveGreen <olivegreenkr98@gmail.com>
2023-05-24 20:08:02 +0900

    ﻿################# feat :  MakingPacketToSend
    패킷에 들어갈 데이터 (dict)를 이용해 패킷을 만드는 함수
    디버깅 필요. 아직 미완

--- src/PacketUtil.py
+++ src/PacketUtil.py
@@ -1,19 +1,28 @@
 ﻿from typing import TypeVar, Tuple, List
-import json
+import json, io
+
+
+def ConcatStr( src : str , dst : any, seperator :  str = '') -> str :
+    ret = []
+    ret.append(src)
+
+    if (type(dst) is Tuple) :
+        for i in dst : 
+            ret.append(str(i))
+    else :
+        ret.append(str(dst))
+
+    ret = seperator.join(ret)
+    return ret
 
 '''
 @기능
     TCP통신 시스템에서 패킷에 관련된 사항을 전체적으로 관리하는 클래스
     패킷을 파싱하고 형식에 따라 나누어 가지고 있으며, 다양한 편의기능을 제공한다.
-@ 생성자
-    기본생성자 인자없음'''
-class cPacketController:
-    __packetContent : dict = None
-
+@생성자 인자 없음'''
+class cPacketManager:
     def __init__(self):
         return
-
-
     '''
     @기능
         패킷을 형식에 따라 Header, Body로 나눈다. 패킷의 유효성 검사도 동시에 진행
@@ -22,11 +31,13 @@ class cPacketController:
     def ParsingPacket(self,packet : str ) -> dict : 
         headerSize = self.__CheckingHeader(packet)
         parsingResult = None
+
         if (headerSize == -1): 
             return parsingResult
 
+        packetBody = packet[ headerSize : -4 ] #\r\n\r\n =  4 bytes 
 
-        
+        parsingResult =  self.__JsonsToDict(packetBody)
         return parsingResult
     
     '''
@@ -36,48 +47,67 @@ class cPacketController:
         - dataToSend : 패킷의 body에 들어갈 내용
     @ret 
         Tuple[bool, str] : [실행 성공여부, 만들어진 패킷]'''
-    def MakingPacketToSendClient(self, dataToSend : dict) -> Tuple[bool, str] :
-        #todo : dict to json str
+    def MakingPacketToSend(self, dataToSend : dict) -> Tuple[bool, str] :
+        
+        packetList = []
+
+        packetBody = json.dumps(sendingContent, indent = 0)
+        packetBody = packetBody.replace(' ','') #body
+
+        bodySize = len(packetBody)
+        
+        packetHeader = ConcatStr("H:",bodySize) #header
+
+        packetList.append("\r\n") #tail headeer
+
+        dataToSend = ConcatStr('',packetList, "\r\n")
+
         return True , dataToSend
     
     '''
     @기능
-        가장 최근에 파싱한 패킷의 contents를 dictionary 형태로 return한다.
+        패킷의 헤더를 읽고 패킷의 유효성 검사 이후 헤더 사이즈를 반환
+    @인자
+        -packet : whole packet
     @ret
-        dictionary : 가장 최근 패킷에 들어 있었던 데이터'''
-    def GetContents(self) -> dict :
-        return self.__packetContent
-
-    '''
-    @기능
-    @인자'''
-    def __CheckingHeader(self, buf : str) -> bool :
+        int :  header size, if packet is abnormal, returns -1'''
+    def __CheckingHeader(self, packet : str) -> int :
+        headerSize = -1
         # 1. 첫줄 -> H, bodySize 확인
+        buf = io.StringIO(packet)
+        firstLine = buf.readline()
+
         # 2. size이용해서 Tail Header검사
-        return True
+
+        return headerSize
     
     '''
     @기능
-        json형식의 str을 
+        json형식의 str을 dict로 변환하여 리턴
     @인자
         -buf : json형태의 str 
     @ret
-        bool : true is success '''
-    def __ParsingBody(self,jsons : str) -> bool:
-
-        #<for Test>
-        jsons = "{\"1\":\"10\",\"2\":\"3\"}"        #json data to test
-        #buf = "[\"1\":\"10\",\"2\":\"3\"]"         #not json
-
+        dict : if None, it is failed '''
+    def __JsonsToDict(self,jsons : str) -> dict:
+        results  = None
         try : 
-            self.__packetContent = json.loads(jsons)
-            return True
+            results = json.loads(jsons)
+            return results
 
         except ValueError: #parsing fail
-            return False
+            return None
 
         
 
 
 
   
+'''test code '''
+
+if __name__ == '__main__':
+    packetManager = cPacketManager()
+
+    sendingContent = { "1" : "10", "2" : "3" }
+    rsult, packetBody = packetManager.MakingPacketToSend(sendingContent)
+    print(packetBody)
+
