9260c28975a0fe3be6bbf0eb1fd081c04da02990
OliveGreen <olivegreenkr98@gmail.com>
2023-05-24 21:38:53 +0900

    feat : PacketUtills::MakingPacketToSend test : MakingPacketToSend docs: .gitmessage.txt
        -json str -> packet 변환 성공
     dictionary 형태로 패킷 바디를 넘기면 클라이언트에게 보낼 패킷내용으로 만들어준다.
       -git message  오타 수정
    
    writer: Jeong﻿

---
+++ src/utils.py
@@ -0,0 +1,20 @@
+﻿'''
+@기능
+    src의 뒤에 dst를 추가하여 하나의 문자열로 만들어준다
+@인자
+    -src : 합쳐진 문자열의 처음
+    -dst : src 뒤에 합쳐질 문자열, list도 가능하다.
+@ret
+    str :  합쳐진 하나의 문자열 '''
+def ConcatStr( src : str , dst : any, seperator :  str = '') -> str :
+    ret = []
+    ret.append(src)
+
+    if (type(dst) is list) :
+        for i in dst : 
+            ret.append(str(i))
+    else :
+        ret.append(str(dst))
+
+    ret = seperator.join(ret)
+    return ret
